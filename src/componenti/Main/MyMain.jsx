b80995d8324e09d7f039de31b286990582531655
samir <samirsadriuboolean@gmail.com>
2025-10-09 17:03:41 +0200

    SECONDA MILESTONE

--- src/componenti/Main/MyMain.jsx
+++ src/componenti/Main/MyMain.jsx
@@ -1,6 +1,7 @@
 import "./MyMain.css"
 import { useContext, useEffect, useState } from "react"
 import { RicercaContext } from "../Context"
+import Flag from "react-flagpack";
 
 const MyMain = () => {
 
@@ -13,6 +14,7 @@ const MyMain = () => {
     const URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${valore}&language=it-IT`;
     const URL2 = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${valore}&language=it-IT`;
     const [film, impostaFilm] = useState([]);
+
 
     useEffect(() => {
 
@@ -31,14 +33,30 @@ const MyMain = () => {
         <main>
             <h1>Risultati della Ricerca: </h1>
 
-            {film.map((movie) => (
-                <ul key={movie.id} className="movie_box">
-                    <li>{movie.title || movie.name}</li>
-                    <li>{movie.original_title || movie.original_name}</li>
-                    <li>{movie.original_language}</li>
-                    <li>{movie.vote_average}</li>
-                </ul>
-            ))}
+            {film.map((movie) => {
+                const codicePaese = (movie.original_language || '').substring(0, 2).toUpperCase();
+                const lingua_bandiera_link = {
+                    IT: "https://flagcdn.com/16x12/it.png",
+                    EN: "https://flagcdn.com/16x12/gb.png",
+                    NL: "https://flagcdn.com/16x12/nl.png",
+                    FR: "https://flagcdn.com/16x12/fr.png",
+                    ES: "https://flagcdn.com/16x12/es.png",
+                    DE: "https://flagcdn.com/16x12/de.png",
+                    JA: "https://flagcdn.com/16x12/jp.png",
+                    ZH: "https://flagcdn.com/16x12/cn.png",
+                    KO: "https://flagcdn.com/16x12/kr.png",
+                    VI: "https://flagcdn.com/16x12/vn.png"
+                };
+                
+                return (
+                    <ul key={movie.id} className="movie_box">
+                        <li>{movie.title || movie.name}</li>
+                        <li>{movie.original_title || movie.original_name}</li>
+                        <li><img src={lingua_bandiera_link[codicePaese]} alt={movie.original_language} width={20} height={10} /></li>
+                        <li>{movie.vote_average}</li>
+                    </ul>
+                )
+            })}
         </main>
     )
 }
