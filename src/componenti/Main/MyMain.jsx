18345f7fc657ccc8039e85f9cb4224fbde42c382
samir <samirsadriuboolean@gmail.com>
2025-10-09 17:59:34 +0200

    per oggi ho finito qua.

--- src/componenti/Main/MyMain.jsx
+++ src/componenti/Main/MyMain.jsx
@@ -1,6 +1,8 @@
 import "./MyMain.css"
 import { useContext, useEffect, useState } from "react"
 import { RicercaContext } from "../Context"
+import placeholder from "../../assets/placeholder.jpg";
+
 // avevo provato a usare reactflag ma non mi ha funzionato. 
 
 const MyMain = () => {
@@ -14,6 +16,10 @@ const MyMain = () => {
     const URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${valore}&language=it-IT`;
     const URL2 = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${valore}&language=it-IT`;
     const [film, impostaFilm] = useState([]);
+
+
+
+
 
 
     useEffect(() => {
@@ -32,31 +38,47 @@ const MyMain = () => {
     return (
         <main>
             <h1>Risultati della Ricerca: </h1>
+            <div className="contenitore">
+                {film.map((movie) => {
+                    const codicePaese = (movie.original_language || '').substring(0, 2).toUpperCase();
+                    const lingua_bandiera_link = {
+                        IT: "https://flagcdn.com/16x12/it.png",
+                        EN: "https://flagcdn.com/16x12/gb.png",
+                        NL: "https://flagcdn.com/16x12/nl.png",
+                        FR: "https://flagcdn.com/16x12/fr.png",
+                        ES: "https://flagcdn.com/16x12/es.png",
+                        DE: "https://flagcdn.com/16x12/de.png",
+                        JA: "https://flagcdn.com/16x12/jp.png",
+                        ZH: "https://flagcdn.com/16x12/cn.png",
+                        KO: "https://flagcdn.com/16x12/kr.png",
+                        VI: "https://flagcdn.com/16x12/vn.png",
+                        CS: "https://flagcdn.com/16x12/cz.png",
+                        PT: "https://flagcdn.com/16x12/pt.png"
+                    };
+                    const immagine = movie.poster_path || movie.backdrop_path
+                            ? `https://image.tmdb.org/t/p/w154/${movie.poster_path || movie.backdrop_path}`
+                            : placeholder;
+                    return (
 
-            {film.map((movie) => {
-                const codicePaese = (movie.original_language || '').substring(0, 2).toUpperCase();
-                const lingua_bandiera_link = {
-                    IT: "https://flagcdn.com/16x12/it.png",
-                    EN: "https://flagcdn.com/16x12/gb.png",
-                    NL: "https://flagcdn.com/16x12/nl.png",
-                    FR: "https://flagcdn.com/16x12/fr.png",
-                    ES: "https://flagcdn.com/16x12/es.png",
-                    DE: "https://flagcdn.com/16x12/de.png",
-                    JA: "https://flagcdn.com/16x12/jp.png",
-                    ZH: "https://flagcdn.com/16x12/cn.png",
-                    KO: "https://flagcdn.com/16x12/kr.png",
-                    VI: "https://flagcdn.com/16x12/vn.png"
-                };
-
-                return (
-                    <ul key={movie.id} className="movie_box">
-                        <li>{movie.title || movie.name}</li>
-                        <li>{movie.original_title || movie.original_name}</li>
-                        <li><img src={lingua_bandiera_link[codicePaese]} alt={movie.original_language} width={20} height={10} /></li>
-                        <li>{movie.vote_average}</li>
-                    </ul>
-                )
-            })}
+                        <ul key={movie.id} className="movie_box">
+                            <li>
+                                <img
+                                    className="POSTER"
+                                    src={immagine}
+                                    alt={`immagine, ${movie.title || movie.name}`} />
+                            </li>
+                            <li className="info_box">
+                                <ul>
+                                    <li>{movie.title || movie.name}</li>
+                                    <li>{movie.original_title || movie.original_name}</li>
+                                    <li><img src={lingua_bandiera_link[codicePaese]} alt={movie.original_language} width={20} height={10} /></li>
+                                    <li>{movie.vote_average}</li>
+                                </ul>
+                            </li>
+                        </ul>
+                    )
+                })}
+            </div>
         </main>
     )
 }
